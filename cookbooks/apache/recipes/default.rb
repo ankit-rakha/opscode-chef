e38a498491dedb05dea2bf2796d53493f2cac18c
Ankit Rakha <ankit.rakha@gmail.com>
2014-01-12 05:06:01 -0800

    added attributes, cleaner cookbook

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -7,22 +7,10 @@
 # All rights reserved - Do Not Redistribute
 #
 
-package_name = "apache2"
-service_name = "apache2"
-document_root = "/var/www"
-
-if node["platform"] == "centos"
-
-	package_name = "httpd"
-	service_name = "httpd"
-	document_root = "/var/www/html"
-
-end
-
 # install apache -- chef will not install
 # package if it is already installed
 
-package package_name do
+package node["package_name"] do
 
 	action :install
 
@@ -31,7 +19,7 @@ end
 # start the apache service
 # service starts on restart
 
-service service_name do
+service node["service_name"] do
 
 	action [:start, :enable]
 
@@ -39,14 +27,7 @@ end
 
 # write to web page
 
-#cookbook_file "#{document_root}/index.html" do
-
-#	source "index.html"
-#	mode "0644"
-
-#end
-
-template "#{document_root}/index.html" do
+template "#{node["document_root"]}/index.html" do
 
         source "index.html.erb"
         mode "0644"
